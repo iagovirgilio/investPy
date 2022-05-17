f8186295846480156edb3eeeb472f0796765640e
Iago Virgílio <iagovirgilio@gmail.com>
2022-05-17 11:35:03 -0300

    ADD: days of dates

--- cotacao/models.py
+++ cotacao/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Cotacao(models.Model):
     moeda = models.CharField(max_length=10)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
 
     def __str__(self):
         return self.moeda
