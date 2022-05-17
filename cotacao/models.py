00226c70552110bb5243e7a333bd6c0b91db63af
Iago Virgílio <iagovirgilio@gmail.com>
2022-05-16 23:10:53 -0300

    ADD: Request and save price

--- cotacao/models.py
+++ cotacao/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Cotacao(models.Model):
+    moeda = models.CharField(max_length=10)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.moeda
--- cotacao/views.py
+++ cotacao/views.py
@@ -1,4 +1,29 @@
+import requests
 from django.shortcuts import render
+from .models import Cotacao
 
 def cotacao(request):
+    if request.method == 'GET':
+
+        url = "https://api.vatcomply.com/rates?base=USD"
+        response = requests.get(url)
+        response_json = response.json()
+
+        for index in response_json['rates']:
+            if index == 'EUR' or index == 'USD' or index == 'BRL':
+
+                cotacao = Cotacao.objects.filter(moeda=index).first()
+
+                if cotacao:
+                    cotacao.valor = response_json['rates'][index]
+                    cotacao.save()
+                else:
+                    cotacao = Cotacao(moeda=index, valor=response_json['rates'][index])
+                    cotacao.save()
+
+        cotacao_list = Cotacao.objects.all()
+        print('-' * 100)
+        print(cotacao_list, )
+        print('-' * 100)
+
     return render(request, 'cotacao.html')
